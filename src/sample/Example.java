673e100971d27363e0e42c93ace7b6e4235d8bd3
prithivi <prithivi3130@gmail.com>
2022-03-28 23:55:33 +0530

    username

---
+++ src/sample/Example.java
@@ -0,0 +1,29 @@
+package sample;
+
+public class Example {
+	public static void main(String []args) {
+		int a[][] = new int [3][3];
+		a[0][0]=10;
+		a[0][1]=20;
+		a[0][2]=30;
+		a[1][0]=10;
+		a[1][1]=20;
+		a[1][2]=30;	
+		a[2][0]=10;
+		a[2][1]=20;
+		a[2][2]=30;						
+			System.out.println(a[1][1]);
+	for (int i = 0; i < a.length; i++) {
+		for (int j = 0; j < a.length; j++) {
+			System.out.println(a[i][j]);
+		}
+	}
+	}
+	
+	
+								
+								
+								
+								
+								
+}
